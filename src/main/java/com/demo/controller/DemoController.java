a0c1f86a3dd338ac224f7dc0af08ae1ba154d1be
Harry Xu <harry.xv@gmail.com>
2025-05-15 21:28:57 -0400

    update with openAPI

--- src/main/java/com/demo/controller/DemoController.java
+++ src/main/java/com/demo/controller/DemoController.java
@@ -1,5 +1,8 @@
 package com.demo.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +16,10 @@ public class DemoController {
 
     private static final Logger logger = LoggerFactory.getLogger(DemoController.class);
 
+    @Operation(summary = "Retrieve a welcome message", description = "Returns a greeting message including the hostname of the server")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Successful response with hostname and welcome message")
+    })
     @GetMapping
     public ResponseEntity<String> greet() {
         logger.debug("Received GET request to /demo endpoint");
